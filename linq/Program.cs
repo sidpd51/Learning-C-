b1e5c306b11b0b57b2e2ff24f0376507b46c1110
sidpd51 <sidpd510@gmail.com>
2024-05-05 09:08:22 +0530

    first commit

---
+++ linq/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Specify the data source.
+// int[] scores = [97, 92, 81, 60];
+List<int> scores = [97, 92, 81, 60];//syntactical sugar
+
+for (int i = 0; i < scores.Count; i++)
+{
+    if(scores[i]>80){
+        Console.WriteLine($"Found a score over {scores[i]}");
+    }
+}
+
+return;
+
+// Define the query expression.
+IEnumerable<int> scoreQuery =
+    from score in scores
+    where score > 80
+    select score;
+
+// Execute the query.
+foreach (var i in scoreQuery)
+{
+    Console.Write(i + " ");
+}
+
+// Output: 97 92 81
---
+++ list /Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.WebSockets;
+using System.Reflection.Metadata;
+//list of strings 
+var names = new List<string> {"siddharth", "Ana", "Felipe"};
+foreach(var name in names){
+    Console.WriteLine($"Hello {name.ToUpper()}");
+}
+//Add and Remove function in list
+Console.WriteLine();
+names.Add("Maria");
+names.Add("Bill");
+names.Remove("Ana");
+
+//using Count and ToUpper() method
+foreach(var name in names){
+    Console.WriteLine($"Hello {name.ToUpper()}");
+}
+
+Console.WriteLine($"My name is {names[0]}");
+Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
+
+Console.WriteLine($"The list has {names.Count} people in it.");
+
+//using indexOf method
+Console.WriteLine();
+var index = names.IndexOf("Felipe");
+if(index != -1){
+    Console.WriteLine($"The name {names[index]} is at index {index}");
+}
+
+var notFound = names.IndexOf("Not Found");
+Console.WriteLine($"When an item is not found, IndexOf will return {notFound}");
+
+//after sorting
+Console.WriteLine();
+names.Sort();
+foreach(var name in names){
+    Console.WriteLine($"Hello {name.ToUpper()}");
+}
+
+//fibonacci numbers
+var fibonacciNumbers = new List<int> {1,1};
+
+// for(int i =3; i<21; i++){
+//     var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+//     var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+//     fibonacciNumbers.Add(previous+previous2);
+// }
+
+while(fibonacciNumbers.Count<20){
+    var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+    var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+    fibonacciNumbers.Add(previous+previous2);
+}
+
+foreach(var item in fibonacciNumbers){
+    Console.WriteLine(item);
+}
+
+
